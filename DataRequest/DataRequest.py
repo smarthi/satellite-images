e6c361c176163faf66c783d863b1eb59e43b1cf7
Matthieu Guillaumin <matthieg@amazon.com>
2018-03-28 14:53:36 +0200

    Alpha-stage commit of polygon sliding window and downloader.

--- DataRequest/DataRequest.py
+++ DataRequest/DataRequest.py
@@ -2,15 +2,25 @@ import wmsRequest
 import geopediaRequest
 import requestDownload
 
-class S2Request():
+class Request():
+    def get_download_list(self):
+        return self.download_list
+
+    def get_data(self):
+        data_list = requestDownload.download_data(self.download_list,redownload=True,threaded_download=True)
+        return data_list
+
+
+class S2Request(Request):
     """
     Class for managing Sentinel 2 requests using Sentinel Hub's WMS service
     """
+    source = 'wms'
+
     def __init__(self, instance_id, crs=4326, data_format=None, bbox=None, layers=None, time=None,
             width=None, height=None, maxcc=100.):
 
         self.instance_id = instance_id
-        self.source = 'wms'
         self.data_format = data_format
         self.bbox = bbox
         self.layers = layers
@@ -19,50 +29,23 @@ class S2Request():
         self.width = width
         self.height = height
         self.maxcc = maxcc
-
         self.request_is_valid = False
-        self.download_list = []
-
-        self.create_wms_request()
-
-    def create_wms_request(self):
         self.download_list = wmsRequest.get_wms_requests(self)
 
-    def get_download_list(self):
-        return self.download_list
 
-    def get_data(self):
-        data_list = requestDownload.download_data(self.download_list,redownload=True,threaded_download=True)
-
-        return data_list
-
-class TulipFieldRequest():
+class TulipFieldRequest(Request):
     """
     Class for managing Tulip Field requests using Geopedia
 
     Note: date argument is ignored at the moment
     """
-    def __init__(self, bbox, width, height, layer, crs=4326):
+    source = 'geopedia'
 
-        self.source = 'geopedia'
+    def __init__(self, bbox, width, height, layer, crs=4326):
         self.bbox = bbox
         self.layer = layer
         self.crs = crs
         self.width = width
         self.height = height
-
         self.request_is_valid = False
-        self.download_list = []
-
-        self.create_geopedia_request()
-
-    def create_geopedia_request(self):
         self.download_list = geopediaRequest.get_geopedia_request(self)
-
-    def get_download_list(self):
-        return self.download_list
-
-    def get_data(self):
-        data_list = requestDownload.download_data(self.download_list,redownload=True,threaded_download=True)
-
-        return data_list
